f7953b56ee3a998d4db021d8fbd39f3cf9e877f1
AdarshDev <adarsh.sundaram3@gmail.com>
2022-11-01 19:52:47 -0400

    Version 0.3.5

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 #--- Imports ---#
-from audioop import mul
-from functools import update_wrapper
 import turtle as trtl
 import random as rand
+
+import shop # The python file I have created which handles the shop/cosmetics.
 
 #--- Creating objects ---# 
 clickTurtle = trtl.Turtle() # This turtle will be the turtle the player needs to click to get cash.
 cashTurtle = trtl.Turtle() # This turtle shows how much cash total cash the player has.
 upgradeMultiplierTurtle = trtl.Turtle()
 showMultiplierStats = trtl.Turtle()
+
+turtle1 = trtl.Turtle() # These will be the turtle cosmetics.
+turtle2 = trtl.Turtle()
+turtle3 = trtl.Turtle()
 
 #--- Config ---#
 clickTurtle.shape("circle")
@@ -21,12 +25,22 @@ cashTurtle.goto(-80, 300)
 cashTurtle.pendown()
 
 upgradeMultiplierTurtle.penup()
-upgradeMultiplierTurtle.goto(150, 0)
-upgradeMultiplierTurtle.shape("circle")
+upgradeMultiplierTurtle.goto(150, -200)
+upgradeMultiplierTurtle.shapesize(1)
 upgradeMultiplierTurtle.pendown()
+upgradeMultiplierTurtle.write("Click here to upgrade multiplier to earn cash faster!", font=("Ariel", 10, "bold"))
 
+showMultiplierStats.penup()
+showMultiplierStats.hideturtle()
+showMultiplierStats.goto(-60, 270)
+showMultiplierStats.pendown()
+
+turtle1.shape("Circle") # Cosmetics
+turtle2.shape("Square")
+turtle3.shape("Triangle")
+
+#--
 turtleColors = ["Purple", "Blue", "Red", "Green", "Yellow", "Black", "Brown", "Cyan"] # The colors the clickTurtle will appear in. (randomly)
-
 
 #--- Variables ---#
 cash = 0
@@ -56,6 +70,7 @@ def gainCash():
     global multiplier
     cash += multiplier
     writeCash(cash) # Writing the cash after player gets it.
+    writeMultiplier(multiplier)
 
 def writeCash(cash):
     cashTurtle.clear()
@@ -73,9 +88,21 @@ def updateMultiplier(x, y):
         multiplierUpgradePrice *= 3
         writeCash(cash)
 
+    writeMultiplier(multiplier)
+    upgradeMultiplierTurtle.clear()
+    upgradeMultiplierTurtle.write("Total multiplier: " + str(multiplier)+ "x. You need: "+ str(multiplierUpgradePrice)+" dollars To upgrade!", font=("Ariel", 10, "bold"))
+
+def writeMultiplier(multiplier):
+    showMultiplierStats.clear()
+    showMultiplierStats.write(arg=("Total multiplier: "+str(multiplier)+"x"), font=("Ariel", 10, "bold"))
+
 #--- Events ---#
 clickTurtle.onclick(click)
 upgradeMultiplierTurtle.onclick(updateMultiplier)
+
+turtle1.onclick(shop.buyTurtle1)
+turtle2.onclick(shop.buyTurtle2)
+turtle3.onclick(shop.buyTurtle3)
 
 wn = trtl.Screen()
 wn.mainloop()
---
+++ shop.py
@@ -0,0 +1,22 @@
+# This module will be used in main.py
+
+import turtle as trtl
+
+
+#--- Variables ---#
+x = 0
+y = 0
+
+#--- Functions ---#
+def buyTurtle1(cash):
+    pass
+
+def buyTurtle2(cash):
+    pass
+
+def buyTurtle3(cash):
+    pass
+
+def moveTurtle():
+    global x 
+    global y
